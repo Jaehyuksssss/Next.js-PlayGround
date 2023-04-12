fc2a62c4b0af991df5ec710e6a8f565da7793b96
Jaehyuksssss <ljh95310@naver.com>
2023-04-12 14:37:55 +0900

    ADD : Styled-components settings

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "../styles/global-style";
+import { theme } from "../styles/theme";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </RecoilRoot>
+  );
+}
